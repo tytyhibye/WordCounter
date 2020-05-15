d3c5685be8b5698b32844d81d23547ed24fa181a
Tyler Bates <Fondlytyler@gmail.com>
2020-05-15 14:01:37 -0700

    add while loop for UX flow.

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -8,31 +8,45 @@ namespace WordCounterEntry
     public static void Main()
     {
       Logic frontEnd = new Logic();
+      bool run = true;
+      while (run == true)
+      {
+        Console.WriteLine(@"
+            Welcome to my
+      ▄▄▌ ▐ ▄▌      ▄▄▄  ·▄▄▄▄        
+      ██· █▌▐█▪    ▀▄  █·██▪ ██       
+      ██▪▐█▐▐▌ ▄█▀▄ ▐▀▀▄ ▐█· ▐█▌      
+      ▐█▌██▐█▌▐█▌.▐▌▐█ █▌██. ██       
+       ▀▀▀▀ ▀▪ ▀█▄▀▪.▀  ▀▀▀▀▀▀•       
+   ▄▄·       ▄• ▄▌  ▐ ▄▄▄▄▄▄▄▄▄  .▄▄▄  
+  ▐█ ▌▪▪     █▪██▌ █▌▐█ ██  ▀▄▀▀▄▄  █·
+  ██ ▄▄ ▄█▀▄ █▌▐█▌▐█▐▐▌ ▐█.▪▐▀▀▪▄▐▀▀▄ 
+  ▐███▌▐█▌.▐▌▐█▄█▌██▐█▌ ▐█▌·▐█▄▄▌▐█ █▌
+  ·▀▀▀  ▀█▄▀▪ ▀▀▀ ▀▀ █▪ ▀▀▀  ▀▀▀ .▀  ▀");
+        Enter:
+        Console.WriteLine("       Please enter a word:");
+        string inputWord = Console.ReadLine();
+        
+        Console.WriteLine("Now enter a sentence or phrase to see if it contains the entered word:");
+        string inputString = Console.ReadLine();
+        
+        frontEnd.GetInputWord(inputWord);
+        frontEnd.GetInputString(inputString);
+        frontEnd.RepeatCounter();
 
-      Console.WriteLine(@"
-          Welcome to my
-    ▄▄▌ ▐ ▄▌      ▄▄▄  ·▄▄▄▄        
-    ██· █▌▐█▪     ▀▄ █·██▪ ██       
-    ██▪▐█▐▐▌ ▄█▀▄ ▐▀▀▄ ▐█· ▐█▌      
-    ▐█▌██▐█▌▐█▌.▐▌▐█•█▌██. ██       
-     ▀▀▀▀ ▀▪ ▀█▄▀▪.▀  ▀▀▀▀▀▀•       
- ▄▄·       ▄• ▄▌ ▐ ▄ ▄▄▄▄▄▄▄▄ .▄▄▄  
-▐█ ▌▪▪     █▪██▌•█▌▐█•██  ▀▄.▀·▀▄ █·
-██ ▄▄ ▄█▀▄ █▌▐█▌▐█▐▐▌ ▐█.▪▐▀▀▪▄▐▀▀▄ 
-▐███▌▐█▌.▐▌▐█▄█▌██▐█▌ ▐█▌·▐█▄▄▌▐█•█▌
-·▀▀▀  ▀█▄▀▪ ▀▀▀ ▀▀ █▪ ▀▀▀  ▀▀▀ .▀  ▀");
-    Console.WriteLine("       Please enter a word:")
-      string inputWord = Console.ReadLine();
-      
-      Console.WriteLine("Now enter a sentence or phrase to see if it contains the entered word:");
-      string inputString = Console.ReadLine();
-      
-      frontEnd.GetInputWord(inputWord);
-      frontEnd.GetInputString(inputString);
-      frontEnd.RepeatCounter();
+        Console.WriteLine("\n\n\n\tthe word: '" + frontEnd.InputWord + "'");
+        Console.WriteLine("\n\nthe number of occurances in the input sentence: " + frontEnd.WordCount + "!" + "\n\n\tWould you like to try again?\n\t\t[Y/N]");
+        string addNew = (Console.ReadLine().ToLower());
 
-      Console.WriteLine("\n\n\n\tthe word: '" + frontEnd.InputWord + "'");
-      Console.WriteLine("\n\n\tthe number of occurances in the input sentence: " + frontEnd.WordCount + "!");
+        if (addNew == "y")
+        {
+          goto Enter;
+        }
+        else
+        {
+          run = false;
+        }
+      }
     }
   }
 }
